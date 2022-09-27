28c1bbc016fc91b6a83dc943a015ff8192c4fd3b
John Bister <John.Bister@ibm.com>
2022-09-27 11:39:19 -0700

    fix size props

--- src/components/Navigation/AddMotion/AddMotion.js
+++ src/components/Navigation/AddMotion/AddMotion.js
@@ -6,10 +6,10 @@ const AddMotion = (props) => {
   const [isAnimating, setIsAnimating] = useState(props.isAnimating)
 
   let iconStyles = {
-    width: props.width,
-    height: props.height
+    width: props.size,
+    height: props.size
   }
-
+  
   React.useEffect(() => {
     setIsAnimating(props.isAnimating)
   }, [props.isAnimating])
