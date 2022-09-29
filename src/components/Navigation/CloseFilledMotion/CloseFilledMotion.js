f3c15a77c2d1ed94e8f3ac495eb28b33f06a81da
John Bister <John.Bister@ibm.com>
2022-09-28 20:12:50 -0700

    Update CloseFilledMotion.js
    break x into horizontal and vertical

--- src/components/Navigation/CloseFilledMotion/CloseFilledMotion.js
+++ src/components/Navigation/CloseFilledMotion/CloseFilledMotion.js
@@ -21,7 +21,8 @@ const CloseFilledMotion = (props) => {
       <svg style={iconStyles} viewBox='0 0 32 32' className={styles.CloseFilledMotion}>
         <title>close filled</title>
         <path className={styles.CloseFilledCircle} fill="black" d="M16,2C8.2,2,2,8.2,2,16s6.2,14,14,14s14-6.2,14-14S23.8,2,16,2z" />
-        <path className={styles.CloseFilled} fill="white" d="M21.4,23L16,17.6L10.6,23L9,21.4l5.4-5.4L9,10.6L10.6,9l5.4,5.4L21.4,9l1.6,1.6L17.6,16l5.4,5.4L21.4,23z" />      
+        <path className={styles.CloseFilledVertical} fill="white" d="M14.9,24.8v-7.6v-2.3V7.2h2.3v7.6v2.3v7.6H14.9z" />
+        <path className={styles.CloseFilledHorizontal} fill="white" d="M14.9,17.1H7.2v-2.3h7.6h2.3h7.6v2.3h-7.6H14.9z" />      
       </svg>
     </div>
   )
