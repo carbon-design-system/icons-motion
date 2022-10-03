97408a90356d093886eb47bc82d833ab1e9454a7
John Bister <John.Bister@ibm.com>
2022-10-03 08:49:09 -0700

    fix zoom in

--- src/components/Navigation/ZoomInMotion/ZoomInMotion.js
+++ src/components/Navigation/ZoomInMotion/ZoomInMotion.js
@@ -20,9 +20,9 @@ const ZoomInMotion = (props) => {
     <div ref={iconRef} className={`${shouldAnimate}`}>
       <svg style={iconStyles} viewBox='0 0 32 32' className={styles.ZoomInMotion}>
         <title>Zoom in</title>
-        <path className={styles.ZoomInGlass} d="M21.4,20c1.6-2,2.6-4.4,2.6-7c0-6.1-4.9-11-11-11S2,6.9,2,13s4.9,11,11,11c2.6,0,5-0.9,7-2.6l7.6,7.6l1.4-1.4L21.4,20z
-		 M13,22c-5,0-9-4-9-9s4-9,9-9s9,4,9,9C22,18,18,22,13,22z" />
-        <path className={styles.ZoomInPlus} d="M18,12h-4V8h-2v4H8v2h4v4h2v-4h4V12z" />
+        <path vector-effect="non-scaling-stroke" className={styles.ZoomInGlass}  fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" d="M13,3C7.4,3,2.9,7.5,2.9,13S7.4,23,13,23s10.1-4.5,10.1-10S18.6,3,13,3z M28.3,28.3l-7.9-7.9"   />
+        <path className={styles.ZoomInHorizontal} d="M18,12h-4h-2H8v2h4h2h4V12z" />
+        <path className={styles.ZoomInVertical} d="M14,12V8h-2v4v2v4h2v-4V12z" />
       </svg>
     </div>
   )
