a647ae6d2536a964f33539a658eb5347f4d3bd15
Krista Starr <kristastarr@Kristas-MacBook-Pro.local>
2022-10-10 08:49:16 -0700

    Updates

---
+++ precache-manifest.a9ad14b16f8ba81c8ef1ac461c0a84fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5e55932e89d030f9cb86aa000b8f1a1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b875a5dda9458e791a0f",
+    "url": "./static/css/main.0e6c5d4c.chunk.css"
+  },
+  {
+    "revision": "e5eae2a3a2680379f907",
+    "url": "./static/js/2.49684807.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.49684807.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b875a5dda9458e791a0f",
+    "url": "./static/js/main.fe51e0d0.chunk.js"
+  },
+  {
+    "revision": "fe3470787af48b5f1b07",
+    "url": "./static/js/runtime-main.bf61600a.js"
+  }
+]);
