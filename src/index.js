86ccc84d85e0b06a939c04f81480364ed25e2d40
johnbister <john.bister@ibm.com>
2022-10-12 14:40:29 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import './index.scss'
 
 export {  AddMotion, AddAltMotion, AddFilledMotion,  AppsMotion, ArrowUpMotion, ArrowDownMotion, ArrowDownLeftMotion, ArrowDownRightMotion, ArrowLeftMotion, ArrowRightMotion, ArrowUpLeftMotion, ArrowUpRightMotion, ChevronDownMotion, ChevronUpMotion, ChevronLeftMotion, ChevronRightMotion, CaretDownMotion, CaretUpMotion, CaretLeftMotion, CaretRightMotion, CloseMotion, CloseOutlineMotion, CloseFilledMotion, DownToBottomMotion, DraggableMotion, HomeMotion, MenuMotion, OverflowMenuHorizontalMotion, OverflowMenuVerticalMotion, PageFirstMotion, PageLastMotion, SelectWindowMotion, SubtractMotion, SubtractAltMotion,SwitcherMotion, UpToTopMotion, ZoomInAreaMotion, ZoomAreaMotion, ZoomOutAreaMotion, ZoomFitMotion, ZoomInMotion, ZoomOutMotion, ZoomResetMotion } from './components/Navigation'
 
-export {  AddMotion, BookmarkMotion, ExploreMotion } from './components/Operations'
+export {  BookmarkMotion, ExploreMotion } from './components/Operations'
