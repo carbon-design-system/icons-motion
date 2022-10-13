8a11de1a4c38c763f51f8e9cdc4787a8e4ef2c0e
johnbister <john.bister@ibm.com>
2022-10-13 14:54:29 -0700

    Update BookmarkFilledMotion.js

--- src/components/Operations/BookmarkFilledMotion/BookmarkFilledMotion.js
+++ src/components/Operations/BookmarkFilledMotion/BookmarkFilledMotion.js
@@ -25,18 +25,16 @@ const BookmarkFilledMotion = (props) => {
       >
         <title>bookmark filled</title>
         <path
-          className={styles.BookmarkFilledBottom}
+          className={styles.BookmarkFilledBottom} fill="black"
           d="M26,4v12H6V4c0-1.1,0.9-2,2-2h16C25.1,2,26,2.9,26,4z"
         />
         <path
-          className={styles.BookmarkFilledTop}
-         d="M26,7v21l-10-5.1L6,28V7H26z"
+          className={styles.BookmarkFilledTop} fill="black"
+          d="M26,7v21l-10-5.1L6,28V7H26z"
         />
       </svg>
     </div>
   )
 }
- 
-  
 
 export default BookmarkFilledMotion
