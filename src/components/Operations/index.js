c47927797b48c46c2062805c003fdd51c6cfa135
johnbister <john.bister@ibm.com>
2022-10-17 12:11:41 -0700

    Update index.js

--- src/components/Operations/index.js
+++ src/components/Operations/index.js
@@ -2,8 +2,9 @@ import BookmarkMotion from './BookmarkMotion';
 import BookmarkAddMotion from './BookmarkAddMotion';
 import BookmarkFilledMotion from './BookmarkFilledMotion';
 import ExploreMotion from './ExploreMotion';
+import FlagMotion from './FlagMotion';
 
 
-export { BookmarkMotion, BookmarkAddMotion, BookmarkFilledMotion, ExploreMotion };
+export { BookmarkMotion, BookmarkAddMotion, BookmarkFilledMotion, ExploreMotion, FlagMotion };
 
 
