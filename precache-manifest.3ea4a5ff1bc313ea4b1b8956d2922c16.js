23c67dc494d92833406fd120b1ebc983ee97eb93
Krista Starr <kristastarr@Kristas-MacBook-Pro.local>
2022-11-16 11:46:03 -0800

    Updates

--- precache-manifest.3ea4a5ff1bc313ea4b1b8956d2922c16.js
+++ precache-manifest.3ea4a5ff1bc313ea4b1b8956d2922c16.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d4250ca3164d99827f2f944fc5491d74",
+    "revision": "71597e3437d36329f88c2fe70fd82c73",
     "url": "./index.html"
   },
   {
-    "revision": "810e1ed44dedc742c46a",
-    "url": "./static/css/main.9e1cb08b.chunk.css"
+    "revision": "bf5d997119a7fb620381",
+    "url": "./static/css/main.ec7962b7.chunk.css"
   },
   {
     "revision": "9b7f5ae98047e69c2a03",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.50a0a45c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "810e1ed44dedc742c46a",
-    "url": "./static/js/main.6abecf2b.chunk.js"
+    "revision": "bf5d997119a7fb620381",
+    "url": "./static/js/main.575243cb.chunk.js"
   },
   {
     "revision": "fe3470787af48b5f1b07",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5753917b4509472bd494f23896683134.js"
+  "./precache-manifest.3ea4a5ff1bc313ea4b1b8956d2922c16.js"
 );
 
 self.addEventListener('message', (event) => {
