e311e5c00994035ba5edcfa31ff251541e2f7bc7
johnbister <john.bister@ibm.com>
2022-11-22 10:47:12 -0800

    fix login

--- example/src/components/OperationsSection/OperationsSection.js
+++ example/src/components/OperationsSection/OperationsSection.js
@@ -352,7 +352,7 @@ const OperationsSection = () => {
             onMouseLeave={() => setLoginAnimating(false)}
           >
             <h3>Log in</h3>
-            <icons.LogoutMotion
+            <icons.LoginMotion
               isAnimating={loginAnimating}  
               size={32} />
           </li>
