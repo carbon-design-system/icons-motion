7eba54d931203fb4d2645e4c790f423e8153fc27
Krista Starr <kristastarr@Kristas-MacBook-Pro.local>
2023-02-17 08:58:24 -0800

    vendor prefixes for css animations

--- src/components/ApiConnect/DownloadMotion/DownloadMotion.js
+++ src/components/ApiConnect/DownloadMotion/DownloadMotion.js
@@ -5,23 +5,25 @@ const DownloadMotion = (props) => {
   const iconRef = useRef(null);
  
   return (
-    <div ref={iconRef} className={props.isAnimating ? styles.isAnimating : null}>
-      <svg
-        // TODO: fix viewbox 
-        // viewBox="0 -3 32 33"
-        className={styles.DownloadMotion}
+    <div ref={iconRef} className={ props.isAnimating ? styles.isAnimating : null}
         width={props.size}
         height={props.size}
+        viewBox="0 0 32 32"
       >
-      <title>download</title>
-      <path
-        className={styles.DownloadArrow}
-        d="M26 15l-1.41-1.41L17 21.17V2h-2v19.17l-7.59-7.58L6 15l10 10 10-10z"
-      />
-      <path d="M26 24v4H6v-4H4v4a2 2 0 002 2h20a2 2 0 002-2v-4z" />
-    </svg>
+      <svg className={styles.DownloadMotion}  width={props.size}
+        height={props.size}>
+        <title>download</title>
+        <path
+          className={styles.DownloadArrow}
+          d='M26 15l-1.41-1.41L17 21.17V2h-2v19.17l-7.59-7.58L6 15l10 10 10-10z'
+        />
+        <path d='M26 24v4H6v-4H4v4a2 2 0 002 2h20a2 2 0 002-2v-4z' />
+      </svg>
     </div>
   );
 };
 
 export default DownloadMotion;
+
+
+// viewBox='0 -10 36 44'
