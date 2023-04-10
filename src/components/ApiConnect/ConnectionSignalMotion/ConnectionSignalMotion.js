7248685af05aa91e2e027f1c494ab2884adc60c8
Krista-Starr <krista.starr@ibm.com>
2023-04-10 10:01:28 -0700

    width and height set by size prop instead of css

--- src/components/ApiConnect/ConnectionSignalMotion/ConnectionSignalMotion.js
+++ src/components/ApiConnect/ConnectionSignalMotion/ConnectionSignalMotion.js
@@ -14,19 +14,19 @@ const ConnectionSignalMotion = (props) => {
         <title>connection signal</title>
         <rect x="15" y="12" width="2" height="18" />
         <path
-          className={styles.ConnectionInner}
+          className={styles.ConnectionOuter}
           d="M11.33,18.22a7,7,0,0,1,0-10.44l1.34,1.49a5,5,0,0,0,0,7.46Z"
         />
         <path
-          className={styles.ConnectionInner}
+          className={styles.ConnectionOuter}
           d="M20.67,18.22l-1.34-1.49a5,5,0,0,0,0-7.46l1.34-1.49a7,7,0,0,1,0,10.44Z"
         />
         <path
-          className={styles.ConnectionOuter}
+          className={styles.ConnectionInner}
           d="M8.4,21.8a11,11,0,0,1,0-17.6L9.6,5.8a9,9,0,0,0,0,14.4Z"
         />
         <path
-          className={styles.ConnectionOuter}
+          className={styles.ConnectionInner}
           d="M23.6,21.8l-1.2-1.6a9,9,0,0,0,0-14.4l1.2-1.6a11,11,0,0,1,0,17.6Z"
         />
       </svg>
