b5fefa5bbaef798a57483d450a10b9b6e1ee50ca
Krista-Starr <krista.starr@ibm.com>
2023-04-14 11:34:48 -0700

    further refinements of animations based on feedback

--- src/components/ApiConnect/DownloadMotion/DownloadMotion.js
+++ src/components/ApiConnect/DownloadMotion/DownloadMotion.js
@@ -9,7 +9,10 @@ const DownloadMotion = (props) => {
       <svg className={styles.DownloadMotion}   
         width={props.size}
         height={props.size}
-        viewBox='0 -5 32 35'>
+        // viewBox='0 -5 32 35' // for previous version
+        viewBox='0 0 32 32'
+
+        >
         <title>download</title>
         <path className={styles.DownloadArrow} d='M26 15l-1.41-1.41L17 21.17V2h-2v19.17l-7.59-7.58L6 15l10 10 10-10z'/>
         <path d='M26 24v4H6v-4H4v4a2 2 0 002 2h20a2 2 0 002-2v-4z' />
--- src/components/ApiConnect/GlobeMotion/GlobeMotion.js
+++ src/components/ApiConnect/GlobeMotion/GlobeMotion.js
@@ -8,17 +8,18 @@ const GlobeMotion = (props) => {
     <div ref={iconRef} className={props.isAnimating ? styles.isAnimating : null}>
       <svg
         viewBox="0 0 32 32"
-        className={styles.GlobeMotion}
         width={props.size}
         height={props.size}>
         <title>globe</title>
-      <path className={styles.Globe} d="M14,4c3.9,0,7,3.1,7,7s-3.1,7-7,7s-7-3.1-7-7S10.1,4,14,4 M14,2c-5,0-9,4-9,9s4,9,9,9s9-4,9-9S19,2,14,2z"/>
-      <path className={styles.GlobeBase} d="M28,11c0-3.7-1.5-7.3-4.1-9.9l-1.4,1.4c4.7,4.7,4.7,12.3,0,17c-4.7,4.7-12.3,4.7-17,0l0,0l-1.4,1.4
-  C6.7,23.5,10.3,25,14,25v3h-4v2h10v-2h-4v-3.2C22.9,23.8,28,18,28,11z"/>
-      <circle className={styles.GlobeLine} cx="14" cy="11" r="8"/>
+        <path className={styles.GlobeBase} d="M28,11c0-3.7-1.5-7.3-4.1-9.9l-1.4,1.4c4.7,4.7,4.7,12.3,0,17c-4.7,4.7-12.3,4.7-17,0l0,0l-1.4,1.4
+      C6.7,23.5,10.3,25,14,25v3h-4v2h10v-2h-4v-3.2C22.9,23.8,28,18,28,11z"/> 
+        <path className={styles.CircleHalf} d="M14,19c-4.4,0-8-3.6-8-8s3.6-8,8-8"/>
+     <circle className={styles.Globe} cx="14" cy="11" r="8"/>
+
     </svg>
   </div>
   );
 };
 
 export default GlobeMotion;
+
