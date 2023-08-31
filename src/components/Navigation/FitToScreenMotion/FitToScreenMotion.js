f5b5461f3bf3cf1af0488c9aae9dea6f956e51e7
Drew Glapa <glapadre@msu.edu>
2023-08-31 15:21:11 -0500

    fix: Cleanup FitToScren & extend AddLarge/SubtractLarge motion (#268)

--- src/components/Navigation/FitToScreenMotion/FitToScreenMotion.js
+++ src/components/Navigation/FitToScreenMotion/FitToScreenMotion.js
@@ -27,60 +27,28 @@ const FitToScreenMotion = (props) => {
 
         <path
           className={styles.FitToScreenTopRight}
-          d='M26,4H16v2h10l0,10h2V6C28,4.9,27.1,4,26,4z'
+          d='M26,6v14h2V6c0-0.5-0.2-1-0.6-1.4S26.5,4,26,4H12v2H26z'
         />
         <path
           className={styles.FitToScreenBottomRight}
-          d='M26,16l0,10H16v2h10c1.1,0,2-0.9,2-2V16H26z'
+          d='M26,26H12v2h14c0.5,0,1-0.2,1.4-0.6S28,26.5,28,26V12h-2V26z'
         />
         <path
           className={styles.FitToScreenBottomLeft}
-          d='M6,26V16H4v10c0,1.1,0.9,2,2,2h10v-2H6z'
+          d='M6,26V12H4v14c0,0.5,0.2,1,0.6,1.4S5.5,28,6,28h14v-2H6z'
         />
         <path
           className={styles.FitToScreenTopLeft}
-          d='M6,4C4.9,4,4,4.9,4,6v10h2V6h10V4H6z'
-        />
-
-        <rect
-          className={styles.FitToScreenTopLine}
-          x='8'
-          y='4'
-          width='16'
-          height='2'
-        />
-
-        <rect
-          className={styles.FitToScreenRightLine}
-          x='26'
-          y='8'
-          width='2'
-          height='16'
-        />
-
-        <rect
-          className={styles.FitToScreenBottomLine}
-          x='8'
-          y='26'
-          width='16'
-          height='2'
-        />
-
-        <rect
-          className={styles.FitToScreenLeftLine}
-          x='4'
-          y='8'
-          width='2'
-          height='16'
+          d='M4.6,4.6C4.2,5,4,5.5,4,6v14h2V6h14V4H6C5.5,4,5,4.2,4.6,4.6z'
         />
 
         <path
           className={styles.FitToScreenInsideTop}
-          d='M22 16 24 16 24 8 16 8 16 10 22 10 22 16z'
+          d='M22,16h2V8h-8v2h6V16z'
         />
         <path
           className={styles.FitToScreenInsideBottom}
-          d='M8 24 16 24 16 22 10 22 10 16 8 16 8 24z'
+          d='M8,24h8v-2h-6v-6H8V24z'
         />
 
         <rect id='_Transparent_Rectangle_' fill='none' width='32' height='32' />
